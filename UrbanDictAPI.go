32cd436f6ec7949aa4845b4d4fd00c5e9fdc9742
Denny P Sam <blackstorm.denny@gmail.com>
2018-01-11 23:22:36 +0530

    Implemented struct. Partially implemented the data to be returned

--- UrbanDictAPI.go
+++ UrbanDictAPI.go
@@ -1,35 +1,7 @@
-// package UrbanDict
-
-// import (
-// 	"io/ioutil"
-// 	"log"
-// 	"net/http"
-// 	"strings"
-
-// 	"golang.org/x/net/html"
-// 	"gopkg.in/xmlpath.v2"
-// )
-
-// //FetchDef returns the results in an array
-// func FetchDef(word string) []byte {
-// 	baseURL := "https://www.urbandictionary.com/define.php?"
-// 	resp, err := http.Get(baseURL + word)
-// 	if err != nil {
-// 		log.Print("err")
-// 		log.Print(err)
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Print(err)
-// 	}
-// 	return body
-// }
-
-package UrbanDict
+package urbandict
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -39,34 +11,64 @@ import (
 
 //FetchDef returns the results in an array
 func FetchDef(word string) []string {
-	baseURL := "https://www.urbandictionary.com/define.php?"
+	baseURL := "https://www.urbandictionary.com/define.php?term="
+
+	//http.Get returns a Response object and error if any
 	resp, err := http.Get(baseURL + word)
 	if err != nil {
-		log.Print("err")
 		log.Print(err)
 	}
+	//closing resp.Body using defer
 	defer resp.Body.Close()
 
-	//read, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//reader = strings.NewReader(read)
+	//the response body is parsed using the xmlpath package
 	xmlroot, xmlerror := xmlpath.ParseHTML(resp.Body)
 	if xmlerror != nil {
 		log.Fatal(err)
 	}
 
-	xpath1 := `//a[@class="word"]`
-	path1 := xmlpath.MustCompile(xpath1)
+	//xpaths defined and compiled
+	xpathMeaning := `//div[@class="meaning"]`
+	xpathUpCount := `//div[@class="count"]/a[@class="up"]`
+	xpathDownCount := `//div[@class="count"]/a[@class="down"]`
 
-	iter := path1.Iter(xmlroot)
-	var body []string
-	for iter.Next() {
-		fmt.Println(iter.Node().String())
-		body = append(body, iter.Node().String())
+	pathMeaning := xmlpath.MustCompile(xpathMeaning)
+	pathUpcount := xmlpath.MustCompile(xpathUpCount)
+	pathDowncount := xmlpath.MustCompile(xpathDownCount)
 
+	//defining JSON structure to be returned
+	type Details struct {
+		meaning   string
+		upcount   string
+		downcount string
 	}
+	var body []string
 
+	//iterated through all the nodes to get all meanings
+	iterMeaning := pathMeaning.Iter(xmlroot)
+	iterUpCount := pathUpcount.Iter(xmlroot)
+	iterDownCount := pathDowncount.Iter(xmlroot)
+
+	var meaning, upCount, downCount []string
+	for iterMeaning.Next() {
+		meaning = append(meaning, iterMeaning.Node().String()) 
+	}
+	for	iterUpCount.Next(){
+		upCount = append(upCount, iterUpCount.Node().String()) 
+	}
+	
+	for	iterDownCount.Next(){
+		downCount = append(downCount, iterDownCount.Node().String()) 
+	}
+		
+	
+		data := Details{meaning, upCount, downCount}
+		b, err := json.Marshal(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(b)
+	}
 	return body
+
 }
